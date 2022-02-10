77442422a11a649feec6ebb74ed587d1797c0bd7
Renaldy <renalfrontend@gmail.com>
2022-02-11 00:15:54 +0700

    Looping data API with new metode in typescript

--- src/project4/pages/person/pagePerson.tsx
+++ src/project4/pages/person/pagePerson.tsx
@@ -2,21 +2,42 @@ import React, { FC } from 'react';
 import {useQuery, UseQueryResult} from 'react-query';
 import { IPerson } from '../../lib/interfaces/IPerson';
 
-const fetchPerson = async (): Promise<IPerson> => {
+const fetchPerson = async (): Promise<IPerson[]> => {
   const res = await fetch('http://localhost:4000/posts');
   return res.json();
 }
 
+interface Propss {
+  listPerson: IPerson[] | undefined
+}
+
+const ListPerson: React.FC<Propss> = ({listPerson}) => {
+  return (
+    <div>
+      {listPerson?.map(item => (
+        <div key={item.id}>
+          <p>{item.id}</p>
+          <p>Name: {item.name}</p>
+          <p>Age: {item.age}</p><br/>
+        </div>
+      ))}
+    </div>
+  )
+}
+
 const PagePerson: FC = () => {
-  const {status, error, data}: UseQueryResult<IPerson, Error> = useQuery<IPerson, Error>(
+  const {isLoading, isError, status, data} = useQuery<IPerson[], Error>(
     'person', 
     fetchPerson
   )
 
+  if(isLoading) return <h1>Loading....</h1>
+  if(isError) return <h1>Error</h1>
+
   return(
     <div>
       <i>Hello word</i>
-      {console.log(data)}
+      <ListPerson listPerson={data} />
     </div>
   )
 }
